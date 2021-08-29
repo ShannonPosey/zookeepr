8f9085342f087295328eb46d85dcf6d979317459
shannon_p <snp1284kasey@aol.com>
2021-08-29 19:42:55 -0400

    adding POST and validation methods

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
+const fs = require("fs");
+const path = require("path");
 const express = require("express");
+const { animals } = require("./data/animals");
+
 const PORT = process.env.PORT || 3001;
 const app = express();
+
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
-const { animals } = require("./data/animals");
-const fs = require("fs");
-const path = require("path");
+
+
 
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
